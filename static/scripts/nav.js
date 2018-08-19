f68e06819b70b26ec47e5d48475a9b8c19b156b7
Joan Perals <joan.perals@ixds.com>
2018-08-19 23:53:52 +0200

    Add nav element

---
+++ static/scripts/nav.js
@@ -0,0 +1,69 @@
+const activeClass = 'active';
+const navHiddenClass = 'hidden';
+const linksBySectionId = {};
+let nav;
+let secondSection;
+
+window.addEventListener('DOMContentLoaded', function () {
+  nav = document.querySelector('nav');
+  nav.querySelectorAll('a').forEach(link => {
+    let id = link.getAttribute('href');
+    if (id.startsWith('/')) {
+      id = id.slice(1);
+    }
+    if (id.startsWith('#')) {
+      id = id.slice(1);
+    }
+    if (id.length) {
+      linksBySectionId[id] = link;
+    }
+  });
+  secondSection = document.querySelector('section:nth-child(2)');
+});
+
+window.addEventListener('scroll', function () {
+  if (nav) {
+    if (secondSection) {
+      toggleVisibility(nav, secondSection);
+    }
+    updateLinkStates(nav);
+  }
+});
+
+function toggleVisibility (nav, secondSection) {
+  const secondSectionTop = secondSection.getBoundingClientRect().top;
+  const navHeight = nav.clientHeight;
+  if (secondSectionTop <= navHeight) {
+    nav.classList.remove(navHiddenClass);
+  } else {
+    nav.classList.add(navHiddenClass);
+  }
+}
+
+function updateLinkStates (nav) {
+  const sections = document.querySelectorAll('section');
+  let currentSection;
+  for (let i = 0; i < sections.length; i++) {
+    const item = sections[i];
+    const top = item.getBoundingClientRect().top;
+    if (top > window.innerHeight) {
+      break;
+    }
+    if (item.getAttribute('id')) {
+      currentSection = item;
+    }
+  }
+  if (currentSection) {
+    const sectionId = currentSection.getAttribute('id');
+    for (const id in linksBySectionId) {
+      const link = linksBySectionId[id];
+      if (typeof link === 'object') {
+        if (id === sectionId) {
+          link.classList.add(activeClass);
+        } else {
+          link.classList.remove(activeClass);
+        }
+      }
+    }
+  }
+}
