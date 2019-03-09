e2038442fb4530b36bc3b28332b877bc3ee4522d
Joan Perals <joan.perals@ixds.com>
2019-03-09 01:01:52 +0100

    Fine tune lightbox

--- static/scripts/lightbox.js
+++ static/scripts/lightbox.js
@@ -1,7 +1,9 @@
 (function lightbox() {
 
   const imageSelector = '.enlarge';
-  const lightboxId = 'lightbox'
+  const lightboxId = 'lightbox';
+  const lightboxImageClass = 'lightbox-image';
+  const withLightboxClass = 'with-lightbox';
 
   window.addEventListener('DOMContentLoaded',
     function() {
@@ -23,11 +25,14 @@
 
   function openLightbox(imageElement) {
     if (imageElement.src) {
-      const lightbox = getLightbox()
-      lightbox.style.backgroundImage = `url(${imageElement.src})`;
+      const lightbox = getLightbox();
+      setTimeout(() => {
+        lightbox.style.opacity = '1';
+      }, 0);
       lightbox.addEventListener('click', closeLightbox);
-      document.body.appendChild(lightbox);
-      document.body.classList.add('with-lightbox');
+      const lightboxImage = getLightboxImage()
+      lightboxImage.style.backgroundImage = `url(${imageElement.src})`;
+      document.body.classList.add(withLightboxClass);
     }
   }
 
@@ -37,13 +42,25 @@
       lightbox = document.createElement('div');
       lightbox.id = lightboxId;
       lightbox.setAttribute('title', 'Click anywhere to close');
+      document.body.appendChild(lightbox);
     }
     return lightbox;
   }
 
+  function getLightboxImage() {
+    const lightbox = getLightbox();
+    let imageElement = lightbox.querySelector(lightboxImageClass);
+    if (!imageElement) {
+      imageElement = document.createElement('div');
+      imageElement.classList.add(lightboxImageClass)
+      lightbox.appendChild(imageElement);
+    }
+    return imageElement;
+  }
+
   function closeLightbox() {
     const lightbox = getLightbox();
-    document.body.classList.remove('with-lightbox');
+    document.body.classList.remove(withLightboxClass);
     document.body.removeChild(lightbox);
   }
 
