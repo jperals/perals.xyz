b777edc5fac85d9a82b6a8e3fcb3ef80e1d1837f
Joan Perals <joan@perals.io>
2020-11-28 22:24:55 +0100

    Add "changing text" effect

---
+++ static/scripts/changing-text.js
@@ -0,0 +1,15 @@
+const elements = document.querySelectorAll('[data-changingtext]');
+for (const el of elements) {
+  const alternativeTexts = el.getAttribute('data-changingtext').split(',');
+  let index = 0;
+  setInterval(function() {
+    el.classList.add('changing-text');
+    setTimeout(function() {
+      el.innerHTML = alternativeTexts[index];
+      el.classList.remove('changing-text');
+    }, 500);
+    index ++;
+    index = index % alternativeTexts.length;
+  },
+  5000);
+}
